af1b3d75f3f469038a9ab5353afceea56721ad89
xxu52 <xxu52@mir.cs.illinois.edu>
2015-08-31 19:22:35 -0500

    Finish ShellExecutor

--- src/main/java/edu/illinois/cs425_mp1/types/Message.java
+++ src/main/java/edu/illinois/cs425_mp1/types/Message.java
@@ -2,9 +2,13 @@ package edu.illinois.cs425_mp1.types;
 
 public abstract class Message {
 	
+	protected Command c;
 	protected String body;
 	public Message(String body) {
 		this.body = body;
+	}
+	public String getBody() {
+		return this.body;
 	}
 
 }
