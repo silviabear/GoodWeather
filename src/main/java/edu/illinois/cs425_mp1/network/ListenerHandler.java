2f217c7183654be9e9922ceebf569328fb983b0c
silihui2 <silihui2@illinois.edu>
2015-09-03 11:52:17 -0500

    Updated ListenerHandler, changed the type definition

--- src/main/java/edu/illinois/cs425_mp1/network/ListenerHandler.java
+++ src/main/java/edu/illinois/cs425_mp1/network/ListenerHandler.java
@@ -1,19 +1,25 @@
 package edu.illinois.cs425_mp1.network;
 
 
+import edu.illinois.cs425_mp1.types.Request;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelPipeline;
 
 /**
+ * This Class is the listener handler class that handles all incoming messages
  * Created by Wesley on 8/31/15.
  */
 public class ListenerHandler extends ChannelInboundHandlerAdapter {
 
+
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) {
             // Discard the received data silently.
+            ChannelPipeline pip = ctx.pipeline();
             ((ByteBuf) msg).release();
+            Request req = (Request) msg;
         }
 
         @Override
