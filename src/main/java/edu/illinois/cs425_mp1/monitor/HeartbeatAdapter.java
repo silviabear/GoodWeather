8483d9cdf6f7ca474b6121a1cbbc1795eaedd281
xxu52 <xxu52@mir.cs.illinois.edu>
2015-10-03 22:03:57 -0500

    -

--- src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatAdapter.java
+++ src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatAdapter.java
@@ -34,7 +34,7 @@ public class HeartbeatAdapter implements Runnable {
 		int i = 0;
 		for (String address : Adapter.getNeighbors()) {
 			NodeStatus status = NodeStatus.NONE;
-			System.out.println("SELFADDRESS" + address);
+			System.out.println("SELFADDRESS" + Adapter.getLocalAddress());
 			if(Adapter.getLocalAddress().equals(address)) {
 				status = NodeStatus.ACTIVE;
 				membershipList.setSelfIndex(i);
