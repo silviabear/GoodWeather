b5522c67b47e6f8d2d6dd419b3792cfa24ede5c8
xxu52 <xxu52@mir.cs.illinois.edu>
2015-10-04 15:59:54 -0500

    -

--- src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatExaminer.java
+++ src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatExaminer.java
@@ -29,8 +29,7 @@ public class HeartbeatExaminer implements Runnable {
 				DateTime time = new DateTime();
 				Node member = HeartbeatAdapter.membershipList.getNode(index);
 				NodeStatus status = member.getStatus();
-				if (status != NodeStatus.ACTIVE
-						|| index == HeartbeatAdapter.membershipList.getSelfId()) {
+				if (index == HeartbeatAdapter.membershipList.getSelfId()) {
 					continue;
 				}
 				if (time.getMillis() - member.getTimeStamp().getMillis() > kickoutInterval) {
