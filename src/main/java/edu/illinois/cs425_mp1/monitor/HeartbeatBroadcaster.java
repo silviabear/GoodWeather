aff1cfd16c8eab56af222f01196b5981bb9bf192
xxu52 <xxu52@mir.cs.illinois.edu>
2015-10-04 21:38:28 -0500

    -

--- src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatBroadcaster.java
+++ src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatBroadcaster.java
@@ -59,13 +59,20 @@ public class HeartbeatBroadcaster implements Runnable {
 	
 	private void broadcast() {
 		HeartbeatAdapter.membershipList.updateSelfTimeStamp();
-		for(int i = 0; i < totalNumNode / broadcastRate; i++) {
+		/*for(int i = 0; i < totalNumNode / broadcastRate; i++) {
 			int index = Math.abs(r.nextInt()) % totalNumNode;
 			if(index == selfId) {
 				i--;
 				continue;
 			}
 			senders[index].send(HeartbeatAdapter.membershipList);
+		}*/
+		for(int i = 0; i < totalNumNode; i++) {
+			if(i == selfId) {
+				i--;
+				continue;
+			}
+			senders[i].send(HeartbeatAdapter.membershipList);
 		}
 	}
 	
