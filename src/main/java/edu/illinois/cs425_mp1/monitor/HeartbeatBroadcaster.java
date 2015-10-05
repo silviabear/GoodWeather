d8134863b5b839066e9bf5c864a84264bd6e0dcc
xxu52 <xxu52@mir.cs.illinois.edu>
2015-10-04 19:07:06 -0500

    -

--- src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatBroadcaster.java
+++ src/main/java/edu/illinois/cs425_mp1/monitor/HeartbeatBroadcaster.java
@@ -62,6 +62,9 @@ public class HeartbeatBroadcaster implements Runnable {
 		Set<Integer> aliveNeighbors = HeartbeatAdapter.membershipList.aliveNeighbors;
 		Integer[] nodes = new Integer[aliveNeighbors.size()];
 		nodes = aliveNeighbors.toArray(nodes);
+		if(nodes.length == 0) {
+			return;
+		}
 		for(int i = 0; i < nodes.length / broadcastRate + 1; i++) {
 			int index = Math.abs(r.nextInt()) % nodes.length;
 			senders[index].send(HeartbeatAdapter.membershipList);
