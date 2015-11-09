57ccc4c7ab6b2257e45b7bbb64ca5bb3074e17e3
silihui2 <silihui2@illinois.edu>
2015-11-08 21:42:01 -0600

    give java larger memory
    Former-commit-id: 5bed71eaad1585e175c312621c9d8a82f61f79e5

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ git config --global credential.helper cache
 rm -rf dfs/*
 rm -rf output/*
 git pull origin master
+export MAVEN_OPTS=-Xmx1024m
 mvn install
 mvn -e exec:java -Xms500m -Xmx1024m -Dexec.mainClass="edu.illinois.cs425_mp1.ui.Console"
