2815daa6859848156ac6af427ad9fffe08c8a99c
silihui2 <silihui2@illinois.edu>
2015-11-08 21:29:39 -0600

    give maven larger memory
    Former-commit-id: 1718bd2eaf02fc850c6b6b7b1d5dfd8eb9bbbf32

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ git config --global credential.helper cache
 rm -rf dfs/*
 rm -rf output/*
 git pull origin master
+export MAVEN_OPTS="-Xmx1024m"
 mvn install
 mvn -e exec:java -Dexec.mainClass="edu.illinois.cs425_mp1.ui.Console"
