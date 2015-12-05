e9b685cff6dc7bc6d02f123911290b990ce8cad0
xxu52 <xxu52@mir.cs.illinois.edu>
2015-12-05 16:18:49 -0600

    -

--- src/main/java/edu/illinois/cs425/g36/app1/CountFinalizer.java
+++ src/main/java/edu/illinois/cs425/g36/app1/CountFinalizer.java
@@ -1,13 +1,30 @@
 package edu.illinois.cs425.g36.app1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import backtype.storm.topology.IRichBolt;
 
 public class CountFinalizer extends IRichBolt {
 
+	Map<String, Integer> counter = new HashMap<String, Integer>();
+	
+	@Override
+	public void cleanup() {
+		for(String port : counter.keySet()) {
+			System.out.println(port + ": " + counter.get(port));
+		}
+	}
+	
 	@Override
 	public void execute(String tuple) {
-		// TODO Auto-generated method stub
-		
+		String[] items = tuple.split(":");
+		String port = items[0];
+		int count = Integer.parseInt(items[1]);
+		if(!counter.containsKey(port)) {
+			counter.put(port, 0);
+		}
+		counter.put(port, counter.get(port) + 1);
 	}
 
 }
