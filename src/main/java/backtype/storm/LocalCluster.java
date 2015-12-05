1dfb9ace444d58d56b69e89f3e0a93bbf4ba9449
xxu52 <xxu52@mir.cs.illinois.edu>
2015-12-04 20:44:52 -0600

    -

--- src/main/java/backtype/storm/LocalCluster.java
+++ src/main/java/backtype/storm/LocalCluster.java
@@ -114,6 +114,7 @@ public class LocalCluster {
 					try {
 						ITuple tuple = collector.nextTuple();
 						outputSender.send(tuple);
+						log.debug("Send tuple " + id);
 						toBeAckedQueue.put(tuple.id, tuple);
 					} catch (InterruptedException e) {
 						break;
