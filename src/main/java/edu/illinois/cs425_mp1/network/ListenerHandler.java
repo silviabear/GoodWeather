9670958214abdc1e23cbd0d41ff96567424e8a53
xxu52 <xxu52@mir.cs.illinois.edu>
2015-12-06 23:42:21 -0600

    -

--- src/main/java/edu/illinois/cs425_mp1/network/ListenerHandler.java
+++ src/main/java/edu/illinois/cs425_mp1/network/ListenerHandler.java
@@ -105,7 +105,8 @@ public class ListenerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         log.error("exception is caught in channel, details printed on console");
-        System.out.println("FAIL");
+        System.out.println(ctx.channel().remoteAddress().toString());
+        LocalCluster.fail(ctx.channel().remoteAddress().toString());
         cause.printStackTrace();
         log.error("closing current channel");
         ctx.close();
